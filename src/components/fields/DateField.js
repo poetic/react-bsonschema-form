c35cc5c5c34c50bd0338cbc1fc8a8e4a589ed58d
Chun-Yang <yang2007chun@gmail.com>
2017-03-31 13:25:48 -0500

    accept options for utc

--- src/components/fields/DateField.js
+++ src/components/fields/DateField.js
@@ -4,6 +4,17 @@ import moment from 'moment';
 
 import 'react-datetime/css/react-datetime.css';
 
+function stringifyDate ({ date, utc }) {
+  if (date == null) {
+    return date
+  }
+  if (utc) {
+    return moment(date).utc().format('L LT')
+  } else {
+    return moment(date).format('L LT')
+  }
+}
+
 function cleanValue (value) {
   if (value && (typeof value.toDate === 'function')) {
     return value.toDate();
@@ -13,17 +24,21 @@ function cleanValue (value) {
 }
 
 function DateField(props) {
-  const {onChange, formData, readonly, disabled} = props;
+  const {onChange, formData, readonly, disabled, uiSchema} = props;
   const {StringField} = props.registry.fields;
+  const {utc} = uiSchema['ui:options'];
 
   if (readonly || disabled) {
+    const formDataString = stringifyDate({ date: formData, utc })
     return <StringField
       {...props}
-      formData={formData && moment(formData).format('L LT')}/>
+      formData={formDataString}/>
   } else {
     return (
       <Datetime
         {...props}
+        utc={utc}
+        input
         timeFormat={false}
         value={formData}
         onChange={(value) => onChange(cleanValue(value))}
